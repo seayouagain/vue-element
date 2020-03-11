db719c4c14e115a120b273a2a6bbe32bd5b71b9c
seayouagain <944247756@qq.com>
2020-03-11 18:10:06 +0800

    Add files via upload
    通过v-model监控数据

--- SelectTree.vue
+++ SelectTree.vue
@@ -3,14 +3,14 @@
     filterable
     multiple
     collapse-tags
-   
+    :disabled="disabled"
     :clearable="clearable"
     :placeholder="placeholder"
     @remove-tag="clearHandle"
      @clear="clearHandle">
-    <el-option :value="option_id" :label="option_label"   :style="[{'height': 'auto','width': 'auto',
+        <el-option :value="option_id"  :label="option_label"   :style="[{'height': 'auto','width': 'auto',
         'padding': '0',
-        'overflow': 'auto'},optionExtendStyle]">
+        'overflow-x': 'scroll'},optionExtendStyle]">
       <div class="treeDiv">
            <el-tree  
             id="tree-option"
@@ -19,8 +19,8 @@
             :accordion="accordion"
             :data="treeData"
             :props="defaultProps"
-            show-checkbox
-            node-key="id" 
+            :show-checkbox="showCheckBox"
+             node-key="id" 
             :check-strictly="true"
             :filter-node-method="filterNode"
             :default-expanded-keys="defaultExpandedKey"
@@ -28,7 +28,6 @@
             @node-click="handleNodeClick">
           </el-tree>   
       </div>
-          
     </el-option>
   </el-select>
 </template>
@@ -45,6 +44,8 @@ export default {
           children: 'children'    // 子级字段名
         }
     },
+    //select组件 disabled
+    disabled:{ type:Boolean, default: false },
     placeholder:{type:String, default: "请选择"},
     // 选项列表数据(树形结构的对象数组)
     treeData:{ type: Array, default: [] },
@@ -52,6 +53,8 @@ export default {
     clearable:{ type:Boolean, default: true },
     // 自动收起
     accordion:{ type:Boolean, default: false },
+    // tree 选择模式checkbox
+    showCheckBox:{type:Boolean, default: true},
     //可配置tree的Style
     treeStyle:{
         type:Object,
@@ -59,13 +62,13 @@ export default {
           return {}
         }
     },
-    
+    //监听v-model
+    value:{type:Array},
     //options 扩展配置
     optionExtendStyle:{
       type:Object,
       default(){
         return {
-          'max-height':'200px' ,
           'max-width': '250px'
         }
       }
@@ -81,26 +84,44 @@ export default {
     }
   },
   mounted(){
+    this.initCheckNode();
   },
 
   methods: {
-  
-    handleNodeClick(node){
-      this.valueTitleArray = []
-      this.checkedNodes = []
-      let nodes = this.$refs.selectTree.getCheckedNodes();
-      nodes.forEach(element=>{
-         let label = element.label
+    initCheckNode(){
+      if (this.value!=null && this.value.length>0){
+        this.checkedNodes = this.value
+      }
+    },
+    handleNodeClick(item, node, self){
+      if(this.showCheckBox){
+        //多选
+        this.valueTitleArray = []
+        this.checkedNodes = []
+        let nodes = this.$refs.selectTree.getCheckedNodes();
+        nodes.forEach(element=>{
+          let label = element.label
+          if(label.length>8){
+            label = label.slice(0,5)+"..."
+          }
+          this.valueTitleArray.push(label)
+          this.checkedNodes.push(element)
+        })
+      }else{
+        //单选
+        this.valueTitleArray = []
+        this.checkedNodes = []
+        let label = node.label
         if(label.length>8){
           label = label.slice(0,5)+"..."
         }
         this.valueTitleArray.push(label)
-        this.checkedNodes.push(element)
-      })
-      this.checkedNodesChange()
+        this.checkedNodes.push(node)
+      }
     },
 
     checkChange(item, node, self) {
+      
       this.valueTitleArray = []
       this.checkedNodes = []
       let nodes = this.$refs.selectTree.getCheckedNodes();
@@ -112,9 +133,7 @@ export default {
         this.valueTitleArray.push(label)
         this.checkedNodes.push(element)
       })
-      this.checkedNodesChange()
     },
-
     // 清除选中
     clearHandle(){
       if(this.valueTitleArray){
@@ -122,7 +141,6 @@ export default {
         this.checkedNodes.pop()
       }
       this.$refs.selectTree.setCheckedNodes(this.checkedNodes)
-      this.checkedNodesChange()
 
     },
   
@@ -133,21 +151,25 @@ export default {
     filterNode(value, data) {
         if (!value) return true;
         return data.label.indexOf(value) !== -1;
-    },
-
-    checkedNodesChange(){
-      this.$emit('getCheckedNodes',this.checkedNodes)
     }
   },
   watch: {
    treeData: {
       deep: true,  // 深度监听
       handler(newVal,oldVal) {
-        if(newVal.length>0){
+        if(newVal.length>0&& this.defaultExpandedKey.length==0){
           this.defaultExpandedKey=[newVal[0].id]
         }
       }
+    },
+    value(newVal) {
+      this.checkedNodes = newVal
+    },
+
+    checkedNodes(newVal){
+       this.$emit('input',this.checkedNodes)
     }
+
   },
   computed:{
 
@@ -157,15 +179,6 @@ export default {
 </script>
 
 <style scoped>
-   /* .el-scrollbar .el-scrollbar__view .el-select-dropdown__item{
-    height: auto;  
-    width: auto;
-    max-height: 150px;
-    max-width: 200px; 
-    padding: 0;
-    overflow: auto; 
-  
-  } */
 
   .el-select-dropdown__item.selected{
     font-weight: normal;
@@ -174,13 +187,7 @@ export default {
     height:auto;
     padding: 0 20px;
   } 
- /* .el-tree-node__label{
-    font-weight: normal;
-  } */
-  .el-tree >>>.is-current .el-tree-node__label{
-    color: #409EFF;
-    font-weight: 700;
-  }
+ 
   .el-tree >>>.is-current .el-tree-node__children .el-tree-node__label{
     color:#606266;
     font-weight: normal;
